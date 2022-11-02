3d84a168e7e96d3c6946b4ecb5d8ee94307808b0
eyucoder <eyueldaniel0921@gmail.com>
2022-11-02 09:07:25 +0100

    output format

--- ip_tracker.py
+++ ip_tracker.py
@@ -14,11 +14,46 @@ args = parser.parse_args()
 
 ip = args.target if args.target else ''
 
+def printer(data):
+    print(
+f'''
+date:           {data["date"]},
+ip address:     {data["ip"]},
+ip range:       {data["ip range"]},
+countryCode:    {data["countryCode"]},
+continent:      {data["continent"]},
+subContinent:   {data["subContinent"]},
+surfaceArea:    {data["surfaceArea"]},
+population:     {data["population"]},
+lifeExpectency: {data["lifeExpectency"]},
+GNP:            {data["GNP"]},
+countryName:    {data["countryName"]},
+region:         {data["region"]},
+regionName:     {data["regionName"]},
+city:           {data["city"]},
+latitude:       {data["latitude"]},
+longitude:      {data["longitude"]},
+timezone:       {data["timezone"]},
+offset:         {data["timezone"]},
+world currency: {data["world currency"]},
+EU member:      {data["EU member"]},
+org:            {data["org"]},
+isp:            {data["isp"]},
+domain:         {data["domain"]},
+usageType:      {data["usageType"]},
+mm_org:         {data["mm_org"]},
+mm_isp:         {data["mm_isp"]},
+i2l_isp:        {data["i2l_isp"]},
+i2l_domain:     {data["i2l_domain"]},
+i2l_type:       {data["i2l_type"]}
+'''
+            )
+
 try:
     response = requests.get(base_url + ip + '?jsonp')
     data = response.json()
     print(response)
-    print(data)
+    printer(data)
 
 except requests.exceptions.Timeout as e:
     print(f'request timeout: {e}')
